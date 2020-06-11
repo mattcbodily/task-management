dd8c946bbe367654ed92af8d52d2b4c7b15180f9
mattcbodily <mattcbodily@gmail.com>
2020-06-11 16:57:46 -0600

    adding styling to dashboard project links

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import {connect} from 'react-redux';
-import {Switch, Route, Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Switch, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import ProjectModal from '../ProjectModal/ProjectModal';
 import TaskDisplay from '../TaskDisplay/TaskDisplay';
+import './Dashboard.scss';
 
 const Dashboard = props => {
     let [projects, setProjects] = useState([]),
@@ -11,8 +12,8 @@ const Dashboard = props => {
 
     const getProjects = () => {
         axios.get(`/api/projects/${props.user.user_id}`)
-        .then(res => setProjects(res.data))
-        .catch(err => console.log(err));
+            .then(res => setProjects(res.data))
+            .catch(err => console.log(err));
     }
 
     useEffect(() => {
@@ -26,22 +27,24 @@ const Dashboard = props => {
                 <p>Welcome back to your workspace!</p>
                 <h3>Projects</h3>
                 {projects.length
-                ? projects.map((project, i) => (
-                    <Link to={`/dashboard/${project.project_id}/tasks`} key={i}>P</Link>
-                ))
-                : (
-                    <>
-                        <p>You don't have any projects!</p>
-                        <button className='create-button' onClick={() => setModalView(true)}>+</button>
-                    </>
-                )}
+                    ? projects.map((project, i) => (
+                        <section className='project-flex'>
+                            <Link className='project-links' to={`/dashboard/${project.project_id}/tasks`} key={i}>{project.project_name.charAt(0)}</Link>
+                        </section>
+                    ))
+                    : (
+                        <>
+                            <p>You don't have any projects!</p>
+                            <button className='create-button' onClick={() => setModalView(true)}>+</button>
+                        </>
+                    )}
                 {modalView
-                ? <ProjectModal id={props.user.user_id} modalFn={setModalView} projectsFn={getProjects}/>
-                : null}                
+                    ? <ProjectModal id={props.user.user_id} modalFn={setModalView} projectsFn={getProjects} />
+                    : null}
             </section>
             <section>
                 <Switch>
-                    <Route exact path='/dashboard/:id/tasks' component={TaskDisplay}/>
+                    <Route exact path='/dashboard/:id/tasks' component={TaskDisplay} />
                 </Switch>
             </section>
         </div>
