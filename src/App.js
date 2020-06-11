bbda1c61c40bce7300fe35ad711395b7aa19a426
mattcbodily <mattcbodily@gmail.com>
2020-06-11 16:17:50 -0600

    installed node-sass, added basic styling to Landing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import routes from './routes';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import {connect} from 'react-redux';
-import {getUser} from '../../redux/userReducer';
+import { connect } from 'react-redux';
+import { getUser } from '../../redux/userReducer';
+import './Landing.scss';
 
 const Landing = props => {
     let [loginView, setLoginView] = useState(true),
@@ -13,79 +14,83 @@ const Landing = props => {
         [viewPassword, setViewPassword] = useState('password');
 
     const login = () => {
-        axios.post('/api/login', {email, password})
-        .then(res => {
-            props.getUser(res.data)
-            props.history.push('/dashboard')
-        })
-        .catch(err => console.log(err));
-    }
-
-    const register = () => {
-        if(password !== '' && password === verPassword){
-            axios.post('/api/register', {firstName, lastName, email, password})
+        axios.post('/api/login', { email, password })
             .then(res => {
                 props.getUser(res.data)
                 props.history.push('/dashboard')
             })
             .catch(err => console.log(err));
+    }
+
+    const register = () => {
+        if (password !== '' && password === verPassword) {
+            axios.post('/api/register', { firstName, lastName, email, password })
+                .then(res => {
+                    props.getUser(res.data)
+                    props.history.push('/dashboard')
+                })
+                .catch(err => console.log(err));
         } else {
             alert("Passwords don't match")
         }
     }
 
     return (
-        <div>
+        <div className='landing'>
             <h1>Welcome to Task Manager</h1>
             {loginView
-            ? (
-                <>
-                    <h3>Log in below</h3>
-                    <input
-                        value={email} 
-                        placeholder='Email'
-                        onChange={(e) => setEmail(e.target.value)}/>
-                    <input
-                        value={password} 
-                        type={viewPassword}
-                        placeholder='Password'
-                        onChange={(e) => setPassword(e.target.value)}/>
-                    <button onClick={login}>Log in</button>
-                    <p>Don't have an account? <span onClick={() => setLoginView(false)}>Register Here</span></p>
-                </>
-            )
-            : (
-                <>
-                    <h3>Create an account</h3>
-                    <input
-                        value={firstName} 
-                        placeholder='First Name'
-                        onChange={(e) => setFirstName(e.target.value)}/>
-                    <input 
-                        value={lastName} 
-                        placeholder='Last Name'
-                        onChange={(e) => setLastName(e.target.value)}/>
-                    <input 
-                        value={email}
-                        placeholder='Email'
-                        onChange={(e) => setEmail(e.target.value)}/>
-                    <input
-                        value={password}
-                        type={viewPassword}
-                        placeholder='Password'
-                        onChange={(e) => setPassword(e.target.value)}/>
-                    <input
-                        value={verPassword} 
-                        type={viewPassword}
-                        placeholder='Verify Password'
-                        onChange={(e) => setVerPassword(e.target.value)}/>
-                    <button onClick={register}>Register</button>
-                    <p>Have an account? <span onClick={() => setLoginView(true)}>Log in here</span></p>
-                </>
-            )}
-            
+                ? (
+                    <>
+                        <h3>Log in below</h3>
+                        <div className='landing-flex'>
+                            <input
+                                value={email}
+                                placeholder='Email'
+                                onChange={(e) => setEmail(e.target.value)} />
+                            <input
+                                value={password}
+                                type={viewPassword}
+                                placeholder='Password'
+                                onChange={(e) => setPassword(e.target.value)} />
+                            <button onClick={login}>Log in</button>
+                            <p>Don't have an account? <span onClick={() => setLoginView(false)}>Register Here</span></p>
+                        </div>
+                    </>
+                )
+                : (
+                    <>
+                        <h3>Create an account</h3>
+                        <div className='landing-flex'>
+                            <input
+                                value={firstName}
+                                placeholder='First Name'
+                                onChange={(e) => setFirstName(e.target.value)} />
+                            <input
+                                value={lastName}
+                                placeholder='Last Name'
+                                onChange={(e) => setLastName(e.target.value)} />
+                            <input
+                                value={email}
+                                placeholder='Email'
+                                onChange={(e) => setEmail(e.target.value)} />
+                            <input
+                                value={password}
+                                type={viewPassword}
+                                placeholder='Password'
+                                onChange={(e) => setPassword(e.target.value)} />
+                            <input
+                                value={verPassword}
+                                type={viewPassword}
+                                placeholder='Verify Password'
+                                onChange={(e) => setVerPassword(e.target.value)} />
+                            <button onClick={register}>Register</button>
+                            <p>Have an account? <span onClick={() => setLoginView(true)}>Log in here</span></p>
+                        </div>
+                    </>
+                )}
+
         </div>
     )
 }
 
-export default connect(null, {getUser})(Landing);
+export default connect(null, { getUser })(Landing);
